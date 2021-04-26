caeb76eb63aaec879041d84f8a7190c3be293a76
Sergey Polyarus <polyarus@gmail.com>
2021-04-26 13:42:00 +0200

    Password validators

--- src/app/core/services/validation.service.ts
+++ src/app/core/services/validation.service.ts
@@ -1,81 +1,35 @@
 import { Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValidationService {
 
-  static getValidatorErrorMessage(validatorName: string, validatorValue?: any) {
-    let config : any = {
-      'required': 'Mandatory',
-      'invalidCreditCard': 'Is invalid credit card number',
-      'invalidEmailAddress': 'Email is not valid',
-      'invalidPassword': 'At least 6 characters',
-      'minlength': `Min length ${validatorValue.requiredLength} characters`,
-      'maxlength': `Max length${validatorValue.requiredLength} characters`,
-      'invalidOldPassword': 'Old password is wrong',
-      'notmatchPasswords': 'Password is not matching',
-      'emailExists': 'Email exists in database',
-      'emailNotExists': 'Email doesn\'t exit id database',
-      'valueExists': 'Login already exists'
-    };
-
-    return config[validatorName];
-  }
-
-  static creditCardValidator(control:any) {
-    // Visa, MasterCard, American Express, Diners Club, Discover, JCB
-    if (control.value.match(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/)) {
-      return null;
-    } else {
-      return { 'invalidCreditCard': true };
-    }
-  }
-
-  static emailValidator(control:any) {
-    // RFC 2822 compliant regex
-    if (control.value && control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
-      return null;
-    } else {
-      return { 'invalidEmailAddress': true };
-    }
-  }
-
-  static emptyValidator(control:any) {
-    // RFC 2822 compliant regex
-    if (control.value && control.value.trim().length > 0) {
-      return null;
-    } else {
-      return { 'required': true };
-    }
-  }
-
-  static passwordValidator(control:any) {
-    // {6,100}           - Assert password is between 6 and 100 characters
-    // (?=.*[0-9])       - Assert a string has at least one number
-    if (control.value && control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
-      return null;
-    } else {
-      return { 'invalidPassword': true };
-    }
-  }
-
-  static matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
-    return (group: FormGroup) => {
-      //let passwordOriginal = group.controls[password];
-      //if (passwordOriginal && passwordOriginal.value.)
-      let passwordInput = group.controls[passwordKey];
-      let passwordConfirmationInput = group.controls[confirmPasswordKey];
-      if ((passwordInput && passwordConfirmationInput) && passwordInput.value !== passwordConfirmationInput.value) {
-       return passwordConfirmationInput.setErrors({'notmatchPasswords': true});
+  static patternValidator(regex: RegExp, error: ValidationErrors): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } => {
+      if (!control.value) {
+        // if control is empty return no error
+        return null;
       }
+  
+      // test the value of the control against the regexp supplied
+      const valid = regex.test(control.value);
+  
+      // if true, return no error (no error), else return error passed in the second parameter
+      return valid ? null : error;
     };
   }
 
 
-
-
-
+  static passwordMatchValidator(control: AbstractControl) {
+    const password: string = control.get('password').value; // get password from our password form control
+    const confirmPassword: string = control.get('confirmPassword').value; // get password from our confirmPassword form control
+    // compare is the password math
+    if (password !== confirmPassword) {
+      // if they don't match, set an error in our confirmPassword form control
+      control.get('confirmPassword').setErrors({ NoPassswordMatch: true });
+    }
+  }
 
 }
--- src/app/features/update-password/update-password.component.ts
+++ src/app/features/update-password/update-password.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/core/services/authentication-service';
 import { RouteStateService } from 'src/app/core/services/route-state.service';
 import { ToastService } from 'src/app/core/services/toast.service';
+import { ValidationService } from 'src/app/core/services/validation.service';
 
 @Component({
   selector: 'app-update-password',
@@ -37,9 +38,25 @@ export class UpdatePasswordComponent implements OnInit {
     this.token = this.route.snapshot.params['token'];
 
     this.forgotForm = this.formBuilder.group({
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: [null, Validators.compose([Validators.required])]
-  });
+
+      password: [ null, Validators.compose([
+          // 1. Password Field is Required
+          Validators.required,
+          // 2. check whether the entered password has a number
+          ValidationService.patternValidator(/\d/, { hasNumber: true }),
+          // 3. check whether the entered password has upper case letter
+          ValidationService.patternValidator(/[A-Z]/, { hasCapitalCase: true }),
+          // 4. Has a minimum length of 8 characters
+          Validators.minLength(8)])
+      ],
+
+        confirmPassword: [null, Validators.compose([Validators.required])]
+    },
+    {
+      // check whether our password and confirm password match
+      validator: ValidationService.passwordMatchValidator
+   }
+    );
 
   }
 
