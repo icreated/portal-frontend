59becc11b7ecfec4118f82ae45ecb1a95708ee0b
Sergey Polyarus <polyarus@gmail.com>
2021-07-05 23:23:30 +0200

    Session Service Test

--- src/app/core/services/menu-data.service.spec.ts
+++ src/app/core/services/menu-data.service.spec.ts
@@ -1,26 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { MenuDataService } from "./menu-data.service";
+import {MenuDataService} from './menu-data.service';
 
-describe("MenuDataService", () => {
-  let service: MenuDataService;
+describe('MenuDataService', () => {
+    let service: MenuDataService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MenuDataService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(MenuDataService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 
-  it('should be not empty', () => {
-    expect(service.getMenuList().length).toBeGreaterThan(0)
-  });
+    it('should be not empty', () => {
+        expect(service.getMenuList().length).toBeGreaterThan(0);
+    });
 
-  it('should have at least home item', () => {
-    const homeItem = service.getMenuList()[0];
-    expect(homeItem.label).toEqual('home');
-  });
+    it('should have at least home item', () => {
+        const homeItem = service.getMenuList()[0];
+        expect(homeItem.label).toEqual('home');
+    });
 
 });
---
+++ src/app/core/services/session.service.spec.ts
@@ -0,0 +1,49 @@
+import {TestBed} from '@angular/core/testing';
+
+import {SessionService} from './session.service';
+
+describe('SessionService', () => {
+    let service: SessionService;
+
+    let store = {} as any;
+    const dummy = {author: 'Orwell', title: '1984', year: 1949};
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(SessionService);
+        store = {} as any;
+        spyOn(sessionStorage, 'getItem').and.callFake((key) => store[key]);
+        spyOn(sessionStorage, 'setItem').and.callFake((key, value) => store[key] = value + '');
+        spyOn(sessionStorage, 'removeItem').and.callFake((key) => {
+            delete store[key];
+        });
+        spyOn(sessionStorage, 'clear').and.callFake(() => {
+            store = {};
+        });
+
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should put & get the same object', () => {
+        service.setItem('book', dummy);
+        expect(service.getItem('book')).toEqual(dummy);
+    });
+
+    it('should put & remove the object, the result is null', () => {
+        service.setItem('book', dummy);
+        service.removeItem('book');
+        expect(service.getItem('book')).toBeNull();
+    });
+
+    it('should clear the session', () => {
+        service.setItem('book1', dummy);
+        service.setItem('book2', dummy);
+        service.clear();
+        expect(service.getItem('book1')).toBeNull();
+        expect(service.getItem('book2')).toBeNull();
+    });
+
+});
