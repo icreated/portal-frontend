5a6a179f9d6033d6db6a06293f64d156e079760a
Sergey Polyarus <polyarus@gmail.com>
2021-07-26 20:41:43 +0200

    Login, UpdatePassword, ForgotPassword Specs

---
+++ src/app/features/update-password/update-password.component.spec.ts
@@ -0,0 +1,122 @@
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {UpdatePasswordComponent} from "./update-password.component";
+import {ToastService} from "../../core/services/toast.service";
+import {AuthenticationService} from "../../core/services/authentication-service";
+import {MessageService} from "primeng/api";
+import {AppCommonModule} from "../../app.common.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterTestingModule} from "@angular/router/testing";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {TranslateModule} from "@ngx-translate/core";
+import {EMPTY} from "rxjs";
+
+
+describe("UpdatePasswordComponent", () => {
+    let component: UpdatePasswordComponent;
+    let fixture: ComponentFixture<UpdatePasswordComponent>;
+    let toastService: ToastService;
+    let authenticationService: AuthenticationService;
+
+    const PASSWORD_OK = 'Orwell1984';
+    const PASSWORD_WITHOUT_CAPITAL = 'orwell1984';
+    const PASSWORD_WITHOUT_NUMBER = 'Orwell';
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UpdatePasswordComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [MessageService],
+            imports: [AppCommonModule, BrowserAnimationsModule, RouterTestingModule.withRoutes([]),
+                HttpClientTestingModule, TranslateModule.forRoot()],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UpdatePasswordComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        toastService = TestBed.inject(ToastService);
+        authenticationService = TestBed.inject(AuthenticationService);
+    });
+
+    describe('updatePassword form validation', () => {
+        it('should render updatePassword input', () => {
+            const compiled = fixture.debugElement.nativeElement;
+            const newPasswordInput = compiled.querySelector('input[formControlName="newPassword"]');
+            expect(newPasswordInput).toBeTruthy();
+            const confirmPasswordInput = compiled.querySelector('input[formControlName="confirmPassword"]');
+            expect(confirmPasswordInput).toBeTruthy();
+        });
+
+        it('should test updatePassword form validity', () => {
+            const form = component.forgotForm;
+            expect(form.valid).toBeFalsy();
+
+            const newPasswordInput = form.controls.newPassword;
+            const confirmPasswordInput = form.controls.confirmPassword;
+            newPasswordInput.setValue('');
+            confirmPasswordInput.setValue('');
+            expect(form.valid).toBeFalsy();
+            newPasswordInput.setValue(PASSWORD_OK);
+            confirmPasswordInput.setValue(PASSWORD_OK);
+            expect(form.valid).toBeTruthy();
+        });
+
+        it('should test updatePassword input validity', () => {
+            const newPasswordInput = component.forgotForm.controls.newPassword;
+            expect(newPasswordInput.valid).toBeFalsy();
+            newPasswordInput.setValue(PASSWORD_WITHOUT_NUMBER);
+            expect(newPasswordInput.valid).toBeFalsy();
+            newPasswordInput.setValue(PASSWORD_WITHOUT_CAPITAL);
+            expect(newPasswordInput.valid).toBeFalsy();
+            newPasswordInput.setValue(PASSWORD_OK);
+            expect(newPasswordInput.valid).toBeTruthy();
+
+            const confirmPasswordInput = component.forgotForm.controls.confirmPassword;
+            expect(confirmPasswordInput.valid).toBeFalsy();
+            newPasswordInput.setValue(PASSWORD_OK);
+            confirmPasswordInput.setValue(PASSWORD_WITHOUT_NUMBER);
+            expect(confirmPasswordInput.valid).toBeFalsy();
+
+            newPasswordInput.setValue(PASSWORD_WITHOUT_NUMBER);
+            confirmPasswordInput.setValue(PASSWORD_WITHOUT_NUMBER);
+            expect(confirmPasswordInput.valid).toBeTruthy();
+            expect(component.f.valid).toBeFalsy();
+
+            newPasswordInput.setValue(PASSWORD_OK);
+            confirmPasswordInput.setValue(PASSWORD_OK);
+            expect(confirmPasswordInput.valid).toBeTruthy();
+        });
+
+        it('should test updatePassword input errors', () => {
+            const newPasswordInput = component.forgotForm.controls.newPassword;
+            expect(newPasswordInput.errors?.required).toBeTruthy();
+            newPasswordInput.setValue(PASSWORD_OK);
+            expect(newPasswordInput.errors).toBeNull();
+            const confirmPasswordInput = component.forgotForm.controls.confirmPassword;
+            confirmPasswordInput.setValue(PASSWORD_OK);
+            expect(confirmPasswordInput.errors).toBeNull();
+        });
+    });
+
+    describe('update', () => {
+        it('should not call backend when form is empty', () => {
+            spyOn(authenticationService, 'passwordValidate').and.returnValue(EMPTY);
+            component.f.newPassword.setValue(PASSWORD_OK);
+            component.f.confirmPassword.setValue('');
+            component.update()
+            expect(component.forgotForm.valid).toBeFalsy();
+            expect(authenticationService.passwordValidate).not.toHaveBeenCalled();
+        });
+
+        it('should call backend when form is provided', () => {
+            component.f.newPassword.setValue(PASSWORD_OK);
+            component.f.confirmPassword.setValue(PASSWORD_OK);
+            spyOn(authenticationService, 'passwordValidate').and.returnValue(EMPTY);
+            component.update()
+            expect(authenticationService.passwordValidate).toHaveBeenCalled();
+        });
+    });
+})
--- src/app/features/update-password/update-password.component.ts
+++ src/app/features/update-password/update-password.component.ts
@@ -13,77 +13,68 @@ import {Message} from 'primeng/api';
 })
 export class UpdatePasswordComponent implements OnInit {
 
-  token = '';
-  forgotForm: FormGroup;
-  loading = false;
-  submitted = false;
-  error = '';
-  msgs: Message[] = [];
+    token = '';
+    forgotForm: FormGroup;
+    loading = false;
+    submitted = false;
+    error = '';
+    msgs: Message[] = [];
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
-              private router: Router, private toastService: ToastService, private authenticationService: AuthenticationService) {
+    constructor(private formBuilder: FormBuilder, private route: ActivatedRoute,
+                private router: Router, private toastService: ToastService, private authenticationService: AuthenticationService) {
 
-      this.forgotForm = this.formBuilder.group({
+        this.forgotForm = this.formBuilder.group({
+                newPassword: [null, Validators.compose([
+                    // 1. Password Field is Required
+                    Validators.required,
+                    // 2. check whether the entered password has a number
+                    ValidationService.patternValidator(/\d/, {hasNumber: true}),
+                    // 3. check whether the entered password has upper case letter
+                    ValidationService.patternValidator(/[A-Z]/, {hasCapitalCase: true}),
+                    // 4. Has a minimum length of 8 characters
+                    Validators.minLength(8)])
+                ],
+                confirmPassword: [null, Validators.compose([Validators.required])]
+            },
+            {validators: [ValidationService.passwordMatchValidator]}
+        );
+    }
 
-          newPassword: [null, Validators.compose([
-          // 1. Password Field is Required
-              Validators.required,
-              // 2. check whether the entered password has a number
-              ValidationService.patternValidator(/\d/, {hasNumber: true}),
-              // 3. check whether the entered password has upper case letter
-              ValidationService.patternValidator(/[A-Z]/, {hasCapitalCase: true}),
-              // 4. Has a minimum length of 8 characters
-              Validators.minLength(8)])
-          ],
+    ngOnInit(): void {
+        // redirect to home if already logged in
+        if (this.authenticationService.currentUserValue) {
+            this.router.navigate(['/']);
+        }
+        this.token = this.route.snapshot.params['token'];
+    }
 
-          confirmPassword: [null, Validators.compose([Validators.required])]
-      },
-      {validators: [ValidationService.passwordMatchValidator]}
-      );
-  }
+    // convenience getter for easy access to form fields
+    get f() {
+        return this.forgotForm.controls;
+    }
 
-  ngOnInit(): void {
-      // redirect to home if already logged in
-      if (this.authenticationService.currentUserValue) {
-          this.router.navigate(['/']);
-      }
-      this.token = this.route.snapshot.params['token'];
+    update() {
+        this.toastService.clear();
+        this.submitted = true;
 
-  }
-
-
-  // convenience getter for easy access to form fields
-  get f() {
-      return this.forgotForm.controls;
-  }
-
-
-  update() {
-
-      this.toastService.clear();
-      this.submitted = true;
-
-      // stop here if form is invalid
-      if (this.forgotForm.invalid) {
-          return;
-      }
-
-      this.loading = true;
-      this.authenticationService.passwordValidate(this.token, this.f.newPassword.value, this.f.confirmPassword.value)
-          .subscribe(
-              () => {
-                  this.forgotForm.reset();
-                  this.router.navigateByUrl('/');
-                  this.toastService.addSingle('success', '', 'password-updated', true);
-              },
-              error => {
-                  this.error = error;
-                  this.loading = false;
-                  this.forgotForm.reset();
-
-                  this.toastService.addSingle('error', '', 'password-not-updated', true);
-
-              });
-  }
+        // stop here if form is invalid
+        if (this.forgotForm.invalid) {
+            return;
+        }
+        this.loading = true;
+        this.authenticationService.passwordValidate(this.token, this.f.newPassword.value, this.f.confirmPassword.value)
+            .subscribe(
+                () => {
+                    this.forgotForm.reset();
+                    this.router.navigateByUrl('/');
+                    this.toastService.addSingle('success', '', 'password-updated', true);
+                },
+                error => {
+                    this.error = error;
+                    this.loading = false;
+                    this.forgotForm.reset();
+                    this.toastService.addSingle('error', '', 'password-not-updated', true);
+                });
+    }
 
 }
